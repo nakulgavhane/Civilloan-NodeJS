0081c7d64f5cd7207d4099935623016baefc539b
Nakul Gavhane <141052501+nakulgavhane@users.noreply.github.com>
2025-01-09 20:45:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,9 +12,12 @@ app.use(cors());
 app.use(express.json()); // Use express.json() instead of body-parser
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect('mongodb+srv://nakulgavhane:nakul123@cluster0.68ada.mongodb.net/civilloan', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected successfully!'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 // Root route
 app.get('/', (req, res) => {
